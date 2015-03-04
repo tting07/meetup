ce5b63851c5c81911e039211027449daaf640f17
uangguan <weiguanhuang@gmail.com>
2015-03-04 23:20:50 +0800

    encode password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
   def signup
-  @user = User.new
+  	  @user = User.new
+  end
+
+  def create
+	  user = User.new(user_params)
+	  user.save
+	  redirect_to :root
+  end
+
+  private
+  def user_params
+    params.require(:user).permit!
   end
 end
