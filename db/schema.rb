173bf3e6bce4ce63eef4e61ebe290efd6721528d
uangguan <weiguanhuang@gmail.com>
2015-03-04 21:41:21 +0800

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150228183549) do
+ActiveRecord::Schema.define(version: 20150304133338) do
 
   create_table "comments", force: true do |t|
     t.text     "content"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 20150228183549) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "content"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
